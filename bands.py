074e835d2fa4d53d86212a90baeb3ea15effa99f
va3dxv <45865432+va3dxv@users.noreply.github.com>
2019-01-02 04:37:27 -0500

    Update bands.py

--- bands.py
+++ bands.py
@@ -4,11 +4,24 @@
 #
 # 01/01/2019
 #
-# Brian Graves - VA3DXV
+# Copyright 2018 Brian Graves - VA3DXV
 #
 # va3dxv@gmail.com
 #
 # https://github.com/va3dxv
+#
+#   This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # pulls band predictions from hamqsl.com
 #
